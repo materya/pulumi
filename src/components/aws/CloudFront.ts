5864ce93fdb3d539f4f825236e2b9efdc2d4c2fa
Sylver <sylver@materya.io>
2020-05-26 09:45:09 +0200

    feat: better zone & domains handling, added options

--- src/components/aws/CloudFront.ts
+++ src/components/aws/CloudFront.ts
@@ -1,17 +1,23 @@
 import * as aws from '@pulumi/aws'
 import * as pulumi from '@pulumi/pulumi'
+import type { cloudfront } from '@pulumi/aws/types/input'
 
 export interface CloudFrontArgs {
   aliases: Array<string>
   bucket: aws.s3.Bucket
   certificateArn: pulumi.Input<string>
+  customErrorResponses?: pulumi.Input<
+    cloudfront.DistributionCustomErrorResponse
+  >[]
+  logs?: boolean
   ttl?: number
+  zoneId: pulumi.Input<string>
 }
 
 export class CloudFront extends pulumi.ComponentResource {
   public readonly cloudFrontDomain: pulumi.Output<string>
 
-  public readonly logsBucketName: pulumi.Output<string>
+  public readonly logsBucketName?: pulumi.Output<string>
 
   public readonly oaiArn: pulumi.Output<string>
 
@@ -24,13 +30,30 @@ export class CloudFront extends pulumi.ComponentResource {
   ) {
     super('materya:aws:CloudFront', name, {}, opts)
 
-    const { aliases, bucket, certificateArn, ttl = 600 } = args
+    const {
+      aliases,
+      bucket,
+      certificateArn,
+      customErrorResponses = [
+        {
+          errorCode: 404,
+          responseCode: 404,
+          responsePagePath: '/404.html',
+        },
+      ],
+      logs = false,
+      ttl = 600,
+    } = args
 
-    const logsBucket = new aws.s3.Bucket(`${name}-logs`, {
-      bucket: pulumi.interpolate`${bucket.bucket}-logs`,
-      acl: 'private',
-      forceDestroy: true,
-    }, { parent: this })
+    let logsBucket: aws.s3.Bucket | undefined
+
+    if (logs) {
+      logsBucket = new aws.s3.Bucket(`${name}-logs`, {
+        bucket: pulumi.interpolate`${bucket.bucket}-logs`,
+        acl: 'private',
+        forceDestroy: true,
+      }, { parent: this })
+    }
 
     const oai = new aws.cloudfront.OriginAccessIdentity(`${name}-oai`, {
       comment: `${name} managed OAI`,
@@ -81,9 +104,7 @@ export class CloudFront extends pulumi.ComponentResource {
         // "100" is the least broad, and also the least expensive.
         priceClass: 'PriceClass_100',
 
-        customErrorResponses: [
-          { errorCode: 404, responseCode: 404, responsePagePath: '/404.html' },
-        ],
+        customErrorResponses,
 
         restrictions: {
           geoRestriction: {
@@ -96,11 +117,13 @@ export class CloudFront extends pulumi.ComponentResource {
           sslSupportMethod: 'sni-only',
         },
 
-        loggingConfig: {
-          bucket: logsBucket.bucketDomainName,
-          includeCookies: false,
-          prefix: pulumi.interpolate`${bucket.bucket}/`,
-        },
+        ...(logsBucket && {
+          loggingConfig: {
+            bucket: logsBucket.bucketDomainName,
+            includeCookies: false,
+            prefix: pulumi.interpolate`${bucket.bucket}/`,
+          },
+        }),
       }))
 
     const distribution = new aws.cloudfront.Distribution(
@@ -109,16 +132,10 @@ export class CloudFront extends pulumi.ComponentResource {
       { parent: this },
     )
 
-    const zone = pulumi.output(bucket.bucket).apply((zoneName: string) => (
-      aws.route53.getZone({
-        name: zoneName,
-      }, { parent: this, async: true })
-    ))
-
     aliases.map(aliase => (
       new aws.route53.Record(`${name}-record-${aliase}`, {
         name: aliase,
-        zoneId: zone.zoneId,
+        zoneId: args.zoneId,
         type: 'A',
         aliases: [
           {
@@ -131,7 +148,7 @@ export class CloudFront extends pulumi.ComponentResource {
     ))
 
     this.cloudFrontDomain = distribution.domainName
-    this.logsBucketName = logsBucket.bucket
+    this.logsBucketName = logsBucket?.bucket
     this.oaiArn = oai.iamArn
     this.targetDomain = pulumi.interpolate`https://${bucket.bucket}`
 
