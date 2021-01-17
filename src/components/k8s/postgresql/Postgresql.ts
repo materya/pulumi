c510af978a621f029e436fd1fbb3ba61f80852ac
Sylver <sylver@materya.io>
2021-01-17 16:29:19 +0100

    feat(postgresql): add PostgresqlUser

---
+++ src/components/k8s/postgresql/Postgresql.ts
@@ -0,0 +1,212 @@
+import * as carbon from '@materya/carbon'
+import * as k8s from '@pulumi/kubernetes'
+import * as postgresql from '@pulumi/postgresql'
+import * as pulumi from '@pulumi/pulumi'
+import * as random from '@pulumi/random'
+
+export type PostgresqlUserCredentials = {
+  [username: string]: pulumi.Input<string>
+}
+
+export interface PostgresqlArgs {
+  chartValuesOverride?: pulumi.Inputs
+  defaults?: {
+    admin?: {
+      password?: pulumi.Input<string>
+      username?: pulumi.Input<string>
+    }
+    databaseName?: string
+    version?: '12.3.0' | '11.8.0' | '10.13.0' | '9.6.18'
+  }
+  labels?: Record<string, string>
+  namespace?: string
+  nodeSelector?: pulumi.Input<Record<string, string>>
+  users: PostgresqlUserCredentials
+}
+
+export class Postgresql extends pulumi.ComponentResource {
+  public readonly adminPassword: pulumi.Output<string>
+
+  public readonly adminUsername: pulumi.Output<string>
+
+  public readonly adminConnectionUrl: pulumi.Output<string>
+
+  public readonly chart: k8s.helm.v2.Chart
+
+  public readonly poolDeployment: pulumi.Output<k8s.apps.v1.Deployment>
+
+  public readonly poolHost: pulumi.Output<string>
+
+  public readonly poolService: pulumi.Output<k8s.core.v1.Service>
+
+  public readonly provider: postgresql.Provider
+
+  public readonly psqlService: pulumi.Output<k8s.core.v1.Service>
+
+  public readonly repmgrPassword: pulumi.Output<string>
+
+  constructor (
+    name: string,
+    args: PostgresqlArgs,
+    opts?: pulumi.ComponentResourceOptions,
+  ) {
+    super('materya:k8s:PostgreSQL', name, {}, opts)
+
+    const chartContext = {
+      name: 'postgresql-ha',
+      repository: 'https://charts.bitnami.com/bitnami',
+      version: '6.3.6',
+    }
+
+    const labels = {
+      provisioner: 'pulumi',
+      module: 'materya',
+      ...args.labels,
+      tier: 'backend',
+      service: 'postgresql',
+      type: 'data',
+    }
+
+    const {
+      chartValuesOverride,
+      defaults: defaultProps = {},
+      nodeSelector,
+      namespace = 'default',
+    } = args
+
+    const {
+      admin: adminProps = {},
+      databaseName = 'postgres',
+      version = '12.3.0',
+    } = defaultProps
+
+    const {
+      password: adminPassword = new random
+        .RandomString(`${name}-adminPassword`, {
+          length: 32,
+          minNumeric: 6,
+          minUpper: 6,
+          special: false,
+        }, { parent: this }).result,
+      username: adminUsername = 'admin',
+    } = adminProps
+
+    this.repmgrPassword = new random.RandomString(`${name}-repmgrPassword`, {
+      length: 32,
+      minNumeric: 6,
+      minUpper: 6,
+      special: false,
+    }, { parent: this }).result
+
+    if (namespace !== 'default') {
+      const _namespace = new k8s.core.v1.Namespace(`namespace-${namespace}`, {
+        metadata: {
+          name: namespace,
+        },
+      }, { parent: this })
+    }
+
+    const pgpoolSecretData = Object.entries(args.users)
+      .reduce((data, user) => (
+        pulumi
+          .all([user[0], user[1], data.usernames, data.passwords])
+          .apply(([username, password, usernames, passwords]) => (
+            {
+              usernames: [username, usernames].filter(Boolean).join(';'),
+              passwords: [password, passwords].filter(Boolean).join(';'),
+            }
+          ))
+      ), pulumi.output({ usernames: '', passwords: '' }))
+
+    const pgpoolSecret = new k8s.core.v1.Secret(`${name}-pgpool-secret`, {
+      stringData: pgpoolSecretData,
+      metadata: {
+        name: `${name}-pgpool-secret`,
+      },
+    }, { parent: this })
+
+    const chartValues = {
+      fullnameOverride: name,
+      pgpool: {
+        adminPassword,
+        nodeSelector,
+        adminUsername,
+        customUsersSecret: pgpoolSecret.metadata.name,
+      },
+      postgresql: {
+        labels,
+        nodeSelector,
+        database: databaseName,
+        password: adminPassword,
+        repmgrPassword: this.repmgrPassword,
+        username: adminUsername,
+        postgresPassword: adminPassword,
+      },
+      postgresqlImage: {
+        tag: version,
+      },
+      persistence: {
+        size: '20Gi',
+      },
+    }
+
+    this.chart = new k8s.helm.v2.Chart(name, {
+      namespace,
+      chart: chartContext.name,
+      fetchOpts: { repo: chartContext.repository },
+      values: chartValuesOverride
+        ? carbon.tools.merge(chartValues, chartValuesOverride)
+        : chartValues,
+      version: chartContext.version,
+    }, { parent: this })
+
+    this.psqlService = this.chart.getResource(
+      'v1/Service',
+      namespace,
+      `${name}-postgresql`,
+    )
+    this.poolService = this.chart.getResource(
+      'v1/Service',
+      namespace,
+      `${name}-pgpool`,
+    )
+
+    this.poolDeployment = this.chart.getResource(
+      'apps/v1/Deployment',
+      namespace,
+      `${name}-pgpool`,
+    )
+
+    this.adminUsername = pulumi.output(adminUsername)
+
+    this.adminPassword = pulumi.secret(adminPassword)
+
+    this.adminConnectionUrl = pulumi
+      .secret(`postgres://${adminUsername}:${adminPassword}@${name}-pgpool/postgres`)
+
+    this.poolHost = pulumi
+      .all([this.poolService.metadata.name])
+      .apply(([poolSvc]) => (`${poolSvc}.${namespace}.svc.cluster.local`))
+
+    this.provider = new postgresql.Provider(`${name}-provider`, {
+      expectedVersion: version,
+      // host: this.poolHost,
+      host: '127.0.0.1',
+      superuser: true,
+      username: adminUsername,
+      password: adminPassword,
+      sslmode: 'disable',
+    }, { parent: this })
+
+    this.registerOutputs({
+      adminConnectionUrl: this.adminConnectionUrl,
+      chart: this.chart,
+      poolDeployment: this.poolDeployment,
+      poolService: this.poolService,
+      poolHost: this.poolHost,
+      provider: this.provider,
+      psqlService: this.psqlService,
+      reprmgrPassword: this.repmgrPassword,
+    })
+  }
+}
---
+++ src/components/k8s/postgresql/PostgresqlUser.ts
@@ -0,0 +1,144 @@
+import * as pulumi from '@pulumi/pulumi'
+import * as postgresql from '@pulumi/postgresql'
+import * as random from '@pulumi/random'
+
+type ObjectType =
+  | 'function'
+  | 'sequence'
+  | 'table'
+  | 'type'
+
+type Privileges = Array<string>
+
+type Grants = Record<ObjectType, Privileges>
+
+const defaultPrivileges: Grants = {
+  table: [
+    'UPDATE',
+    'REFERENCES',
+    // 'TRUNCATE',
+    'SELECT',
+    'DELETE',
+    'TRIGGER',
+    'INSERT',
+  ],
+  sequence: [
+    'USAGE',
+    'SELECT',
+    // 'UPDATE',
+  ],
+  function: [
+    'EXECUTE',
+  ],
+  type: [
+    'USAGE',
+  ],
+}
+
+export interface PostgresqlUserArgs {
+  password?: pulumi.Input<string>
+  privileges?: pulumi.Input<Grants>
+  schema?: pulumi.Input<string>
+  username: pulumi.Input<string>
+}
+
+export class PostgresqlUser extends pulumi.ComponentResource {
+  public readonly name: string
+
+  public readonly password: pulumi.Output<string | undefined>
+
+  public readonly role: postgresql.Role
+
+  public readonly username: pulumi.Output<string>
+
+  constructor (
+    name: string,
+    args: PostgresqlUserArgs,
+    opts?: pulumi.ComponentResourceOptions,
+  ) {
+    super('materya:k8s:PostgresqlUser', name, {}, opts)
+
+    const {
+      password = new random.RandomString(`${name}-password`, {
+        length: 32,
+        minNumeric: 6,
+        minUpper: 6,
+        special: false,
+      }, { parent: this }).result,
+      username,
+    } = args
+
+    const role = new postgresql.Role(`${name}-role`, {
+      password,
+      login: true,
+      name: username,
+    }, { parent: this })
+
+    this.name = name
+    this.username = role.name
+    this.password = role.password
+    this.role = role
+
+    this.registerOutputs({
+      password: this.password,
+      username: this.username,
+    })
+  }
+
+  setPrivileges ({
+    database,
+    owner = this.role.name,
+    privileges = defaultPrivileges,
+    schema = 'public',
+  }: {
+    database: postgresql.Database
+    owner?: pulumi.Input<string>
+    privileges?: Grants
+    schema?: string
+  }): void {
+    Object.keys(privileges).forEach(objectType => {
+      const _privs = new postgresql.DefaultPrivileges(
+        `${this.name}-${objectType}-default-privs`,
+        {
+          database: database.name,
+          privileges: privileges[objectType as ObjectType],
+          schema,
+          objectType,
+          owner,
+          role: this.role.name,
+        },
+        {
+          parent: this,
+          dependsOn: [this.role, database],
+        },
+      )
+
+      if (objectType !== 'type') {
+        const _grants = new postgresql.Grant(
+          `${this.name}-${objectType}-grants`,
+          {
+            database: database.name,
+            privileges: privileges[objectType as ObjectType],
+            schema,
+            objectType,
+            role: this.role.name,
+          },
+          {
+            parent: this,
+            dependsOn: [this.role, database],
+          },
+        )
+      }
+    })
+  }
+
+  getConnectionUrl ({ dbHost, dbName }: {
+    dbHost: pulumi.Input<string>
+    dbName: pulumi.Input<string>
+  }): pulumi.Output<string> {
+    return pulumi.all([this.role.name, this.role.password, dbHost, dbName])
+      .apply(([username, password, host, name]) => (
+        `postgres://${username}:${password}@${host}/${name}`
+      ))
+  }
+}
